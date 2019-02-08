93079d32697a4211d15703a04147166636a9792c
Erik Thiem <ethiem@covermymeds.com>
2019-02-07 22:23:52 -0500

    Add gem and structure view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+  def index
+    @bg_saying = "shit bg says"
+    @bs_saying = "shit bs says"
+    @et_saying = "shit et says"
+    @mb_saying = "shit mb says"
+    @rh_saying = "shit rh says"
+  end
 end
