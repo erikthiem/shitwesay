62cf06832d17662d49f862b6561cdbac225b2423
Erik Thiem <ethiem@covermymeds.com>
2019-02-07 23:03:28 -0500

    One step at a time

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @bg_saying = "shit bg says"
-    @bs_saying = "shit bs says"
-    @et_saying = "shit et says"
-    @mb_saying = "shit mb says"
-    @rh_saying = "shit rh says"
+    @bg_saying = Sentence.generate_saying_for_author("bg")
+    @bs_saying = Sentence.generate_saying_for_author("bs")
+    @et_saying = Sentence.generate_saying_for_author("et")
+    @mb_saying = Sentence.generate_saying_for_author("mb")
+    @rh_saying = Sentence.generate_saying_for_author("rh")
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,3 +1,12 @@
 class Sentence < ApplicationRecord
   has_one :author
+
+  def self.generate_saying_for_author(author_name)
+    author_id = Author.find_by(name: "et")
+
+    data = []
+    if author_id
+      data = Sentence.where(author_id: author_id).pluck(:content)
+    end
+  end
 end
