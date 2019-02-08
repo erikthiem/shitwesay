44560ac827bbd75a092e4bde63e521290627e379
Erik Thiem <ethiem@covermymeds.com>
2019-02-08 07:52:46 -0500

    Add pry and remove 'et'

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,11 +2,11 @@ class Sentence < ApplicationRecord
   has_one :author
 
   def self.generate_saying_for_author(author_name)
-    author_id = Author.find_by(name: "et")
+    author_id = Author.find_by(name: author_name)
 
-    data = []
+    sentences = []
     if author_id
-      data = Sentence.where(author_id: author_id).pluck(:content)
+      sentences = Sentence.where(author_id: author_id).pluck(:content)
     end
   end
 end
